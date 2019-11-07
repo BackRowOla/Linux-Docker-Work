d8874b972d39286f64c83d5f573e6ff5995fe489
BackRowOla <Babalakin.Oyeyemi@academytrainee.com>
2019-11-07 15:41:59 +0000

    added text adventure to docker

---
+++ newDir/morestuff/NewEnemy.java
@@ -0,0 +1,18 @@
+public class NewEnemy extends Unit {
+
+	public NewEnemy(int health, int xPos, int yPos) {
+		super(health, xPos, yPos);
+	}
+
+	public boolean isPlayerNearby(NewPlayer p) {
+		int base = p.getxPos() - this.getxPos();
+		int height = p.getyPos() - this.getyPos();
+		int distance = (int) Math.hypot(base, height);
+
+		if (!(distance >= 1)) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+}
